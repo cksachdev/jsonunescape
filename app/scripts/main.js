8ddb65352d0cbb61f68c56709a21de5d890740c8
Vaibhav <vaibhavbhuva>
2018-05-29 17:22:28 +0530

    Added initial scripts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,24 @@
-;
+var parseContentBody = function(contentBody) {
+  try {
+      contentBody = JSON.parse(contentBody);
+  } catch (e) {
+      contentBody = convertToJSON(contentBody);
+  }
+  return contentBody;
+}
+
+var convertToJSON = function(contentBody) {
+  try {
+      var x2js = new X2JS({ attributePrefix: 'none', enableToStringFunc: false });
+      return x2js.xml_str2json(contentBody);
+  } catch (e) {
+      return;
+  }
+}
+
 
 $(function () {
-  $("#horizontal").kendoSplitter({
+  $('#horizontal').kendoSplitter({
     panes: [{
         collapsible: true,
         resizable: true
@@ -13,11 +30,11 @@ $(function () {
     ]
   });
 
-  $('#copyInput').click(function(){
+  $('#copyInput').click(function () {
     copyToClipboard('jsonInput');
   });
 
-  $('#copyOutput').click(function(){
+  $('#copyOutput').click(function () {
     copyToClipboard('jsonOutput');
   });
 
@@ -25,54 +42,53 @@ $(function () {
 
 
 function copyToClipboard(element_id) {
-  var aux = document.createElement("div");
-  aux.setAttribute("contentEditable", true);
+  var aux = document.createElement('div');
+  aux.setAttribute('contentEditable', true);
   aux.innerHTML = document.getElementById(element_id).value;
-  aux.setAttribute("onfocus", "document.execCommand('selectAll',false,null)"); 
+  aux.setAttribute('onfocus', 'document.execCommand(\'selectAll\',false,null)');
   document.body.appendChild(aux);
   aux.focus();
-  
-  try {  
-    document.execCommand("copy");
-    console.log('Copied');  
+
+  try {
+    document.execCommand('copy');
+    console.log('Copied');
     alert('Copied!');
-  } catch(err) {  
-    console.log('Oops, unable to copy');  
+  } catch (err) {
+    console.log('Oops, unable to copy');
   } finally {
     document.body.removeChild(aux);
   }
 
 
-  window.getSelection().removeAllRanges();  
+  window.getSelection().removeAllRanges();
 }
 
 function minifyJSON(element_id) {
-  var content = $("#"+element_id).val();
+  var content = $('#' + element_id).val();
   if (content.trim().length == 0) {
-      return false;
+    return false;
   }
   try {
-      var jsonStr = content;
-      var jsonObject = $.parseJSON(jsonStr);
-      $('#'+element_id).val(JSON.stringify(jsonObject, null, 0));
-     
+    var jsonStr = content;
+    var jsonObject = $.parseJSON(jsonStr);
+    $('#' + element_id).val(JSON.stringify(jsonObject, null, 0));
+
   } catch (e) {
-      validateJSON();
+    validateJSON();
   }
 }
 
 
 function beautifyJSON(element_id) {
-  var content = $("#"+element_id).val();
+  var content = $('#' + element_id).val();
   if (content.trim().length == 0) {
-      return false;
+    return false;
   }
   try {
-      var jsonStr = content;
-      var jsonObject = $.parseJSON(jsonStr);
-      $('#'+element_id).val(JSON.stringify(jsonObject, null, parseInt(2)));
+    var jsonStr = content;
+    var jsonObject = $.parseJSON(jsonStr);
+    $('#' + element_id).val(JSON.stringify(jsonObject, null, parseInt(2)));
   } catch (e) {
-      validateJSON();
+    validateJSON();
   }
 }
-

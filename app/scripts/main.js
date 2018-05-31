e14955438c034c1ba4db7c57635c0ceb9675d6b2
Vaibhav <vaibhavbhuva>
2018-05-31 17:15:59 +0530

    Added Json to xml functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,11 @@
+const RX_BACKSPACE = /\\b/g;
+const RX_FORM_FEED = /\\f/g;
+const RX_NEWLINE = /\\n/g;
+const RX_CARRIAGE_RETURN = /\\r/g;
+const RX_TAB = /\\t/g;
+const RX_DOUBLE_QUOTE = /\\"/g;
+const RX_BACKSLASH = /\\\\/g;
+
 $(function () {
   $('#horizontal').kendoSplitter({
     panes: [{
@@ -11,26 +19,19 @@ $(function () {
     ]
   });
 
-  $('#copyInput').click(function () {
-    copyToClipboard('jsonInput');
-  });
-
   $('#copyOutput').click(function () {
-    copyToClipboard('jsonOutput');
+    copyToClipboard('hiddenBodyOutput');
   });
 
   Prism.plugins.NormalizeWhitespace.setDefaults({
-    'break-lines': 80
+    'break-lines': 120
   });
-
-
 });
-
 
 function copyToClipboard(element_id) {
   var aux = document.createElement('div');
   aux.setAttribute('contentEditable', true);
-  aux.innerHTML = document.getElementById(element_id).value;
+  aux.innerHTML = document.getElementById(element_id).innerHTML;
   aux.setAttribute('onfocus', 'document.execCommand(\'selectAll\',false,null)');
   document.body.appendChild(aux);
   aux.focus();
@@ -49,47 +50,54 @@ function copyToClipboard(element_id) {
   window.getSelection().removeAllRanges();
 }
 
-function minifyJSON(element_id) {
-  var content = $('#' + element_id).val();
+
+function viewJsonOuput() {
+  var content = $('#jsonInput').val();
   if (content.trim().length == 0) {
     return false;
   }
-  try {
-    var jsonStr = content;
-    var jsonObject = $.parseJSON(jsonStr);
-    $('#' + element_id).val(JSON.stringify(jsonObject, null, 0));
 
-  } catch (e) {
-    validateJSON();
+  try {
+    var jsonStr = unescapeJsonString(content);
+    var jsonObject = $.parseJSON(jsonStr);
+    $("#bodyOutput,#hiddenBodyOutput").empty().text(JSON.stringify(jsonObject, null, parseInt(2)));
+  } catch (error) {
+    $("#bodyOutput,#hiddenBodyOutput").empty().text(content);
   }
+
+  setTimeout(function () {
+    Prism.highlightElement($("#bodyOutput")[0]);
+  }, 10);
+  
 }
 
-function beautifyJSON(element_id) {
-  var content = $('#' + element_id).val();
-  if (content.trim().length == 0) {
-    return false;
-  }
-  try {
-    var jsonStr = content;
-    var jsonObject = $.parseJSON(jsonStr);
-    $('#' + element_id).val(JSON.stringify(jsonObject, null, parseInt(2)));
-  } catch (e) {
-    validateJSON();
-  }
-}
-
-function jsonInputToOutput() {
+function viewXmlOutput() {
   var content = $("#jsonInput").val();
   if (content.trim().length == 0) {
-      return false;
+    return false;
   }
-  try { 
-      var jsondata = content.toString();
-      var converter = new E2EConverter();
-      console.log(converter.buildECML(jsondata, true));
-      $("#output").empty().text(converter.buildECML(jsondata, true));
-  } catch (e) {
-      console.log('error!');
+  var converter = new E2EConverter();
+  try {
+    var enescapeJson = unescapeJsonString(content);
+    var jsondata = JSON.parse(enescapeJson);
+    $("#bodyOutput,#hiddenBodyOutput").empty().text(converter.buildECML(jsondata, true));
+  } catch (error) {
+    var output = converter.buildECML(JSON.parse(content), true);
+    $("#bodyOutput,#hiddenBodyOutput").empty().text(output);
   }
+
+  setTimeout(function () {
+    Prism.highlightElement($("#bodyOutput")[0]); 
+  }, 10);
 }
 
+function unescapeJsonString(text) {
+  text = text.replace(RX_BACKSPACE, "\b");
+  text = text.replace(RX_FORM_FEED, "\f");
+  text = text.replace(RX_NEWLINE, "\n");
+  text = text.replace(RX_CARRIAGE_RETURN, "\r");
+  text = text.replace(RX_TAB, "\t");
+  text = text.replace(RX_DOUBLE_QUOTE, "\"");
+  text = text.replace(RX_BACKSLASH, "\\");
+  return text;
+}
